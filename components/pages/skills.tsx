b2f0bdbeb65cfa38912fbf43c5d479da34ebf4e3
Sam <mcsam1216@gmail.com>
2022-06-18 02:45:47 +0800

    update: add new skill

--- components/pages/skills.tsx
+++ components/pages/skills.tsx
@@ -24,6 +24,8 @@ const SKILLS = [
 	'bootstrap',
 	'materialUI',
 	'chromeExtension',
+	'solanaWeb3',
+	'serumAnchor',
 ]
 
 interface SkillBoxProps {
@@ -34,7 +36,10 @@ const SkillBox: React.FC<SkillBoxProps> = ({ skill }) => {
 	return (
 		<div className="flex flex-row items-center space-x-4 p-4 rounded-lg shadow-sm hover:shadow-lg cursor-pointer bg-slate-100 dark:bg-gray-600 w-[12rem]">
 			<Image src={`/skills/${skill}.svg`} alt={skill} width="30" height="30" />
-			<div className="text-lg flex-1 whitespace-nowrap truncate">
+			<div
+				data-tip={startCase(skill)}
+				className="text-lg flex-1 whitespace-nowrap truncate"
+			>
 				{startCase(skill)}
 			</div>
 		</div>
